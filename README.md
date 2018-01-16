22e37d75987637db4dedf97f769d423e38ceee41
Marc Juul <github@juul.io>
2018-01-15 23:14:21 -0800

    switched to d3. basic line and dots example working

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 
-Experimenting with a pure javascript simulator for [disaster.radio](https://disaster.radio).
+Experimenting with a pure javascript simulator for [disaster.radio](https://disaster.radio). Nothing works yet.
 
+
+# depencies
+
+```
+npm install
+```
+
+# build
+
+```
+npm run build
+```
+
+# develop
+
+```
+npm run watch
+```
+
+# run
+
+```
+npm start
+# open browser to http://localhost:8000
+```
+
+
+# license and copyright
+
+* Copyright 2018 Marc Juul
+* License: AGPLv3
--- index.js
+++ index.js
@@ -1,41 +1,62 @@
 
-var sigma = require('sigma');
 
-console.log("test");
+var d3 = require('d3');
+var c = d3.select('#plot')
+
+/*
+  // experimental pan/zoom (not working well)
+  c.call(d3.zoom().on("zoom", function () {
+    c.attr("transform", d3.event.transform)
+  }))
+*/
 
 
-function test() {
-  var s = new sigma('container');
-  
-  // Then, let's add some data to display:
-  s.graph.addNode({
-    // Main attributes:
-    id: 'n0',
-    label: 'Hello',
-    // Display attributes:
-    x: 0,
-    y: 0,
-    size: 1,
-    color: '#f00'
-  }).addNode({
-    // Main attributes:
-    id: 'n1',
-    label: 'World !',
-    // Display attributes:
-    x: 1,
-    y: 1,
-    size: 1,
-    color: '#00f'
-  }).addEdge({
-    id: 'e0',
-    // Reference extremities:
-    source: 'n0',
-    target: 'n1'
-  });
-  
-  // Finally, let's ask our sigma instance to refresh:
-  s.refresh();
+var dotData = [
+  {
+    x: Math.round(Math.random()*500),
+    y: Math.round(Math.random()*500),
+    radius: 10
+  },
+  {
+    x: Math.round(Math.random()*500),
+    y: Math.round(Math.random()*500),
+    radius: 25
+  }
+];
+
+function drawLine() {
+  c.append("line")
+    .attr('x1', dotData[0].x)
+    .attr('y1', dotData[0].y)
+    .attr('x2', dotData[1].x)
+    .attr('y2', dotData[1].y)
+    .attr('stroke-width', 2)
+    .attr('stroke', 'blue');
 }
 
-test();
+
+function update() {
+  var dots = c.selectAll('circle')
+  
+  var newDots = dots
+    .data(dotData)
+    .enter()
+    .append('circle')
+
+  newDots
+    .attr('cx', function(o) {
+      return o.x;
+    })
+    .attr('cy', function(o) {
+      return o.y;
+    })
+    .attr('r', function(o) {
+      return o.radius;
+    })
+    .style('fill', 'green');
+  
+}
+
+//drawLine();
+update();
 
