4919efbcf0a730cee76b44b2dc2f81df736a904d
paidforby <ggallo102@gmail.com>
2018-09-15 09:46:22 -0700

    test code related to #2

--- simulator.js
+++ simulator.js
@@ -4,11 +4,12 @@ var Network = require('./network.js');
 var process = require('process');
 
 var net = new Network({
-  count: 3, // number of nodes in area
+  count: 1, // number of nodes in area
   width: 2000, // width of area to fill with nodes
   height: 2000, // height of area to fill with nodes
   router: './routers/ping_example',
-  debug: true
+  debug: true,
+  airtime: false
 }, {
 
   // overwrite radio opts here if needed
@@ -18,12 +19,13 @@ var net = new Network({
 
 var node = net.nodes[0];
 console.log("Nodes within range of node 0:", net.nodesInRangeOf(node).length)
-
+/*
 node.tx("hop 0", function(err) {
   if(err) return console.error(err);
 
 //  console.log("Message sent");
 })
+*/
 
 process.on('exit', function () {
   net.kill();
