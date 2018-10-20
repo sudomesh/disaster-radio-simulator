926a9f2c6b9f37e78335679ceb57aff499c012d9
paidforby <ggallo102@gmail.com>
2018-10-19 21:38:27 -0700

    clean up last commit, add nodeID opt, expand buildPacket functionality

--- node.js
+++ node.js
@@ -75,7 +75,7 @@ function Node(opts, radioOpts, routerOpts) {
       var args = this.opts.router.split(/\s+/);
       var cmd = args[0];
       args = args.slice(1);
-      args = args.concat(['-t', this.network.opts.timeDistortion, '-a', this.mac ]);
+      args = args.concat(['-t', this.network.opts.timeDistortion, '-a', this.mac, '-n', this.id ]);
 
       this.router = spawn(cmd, args);
 
