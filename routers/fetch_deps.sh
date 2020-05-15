e89900a48189b8bf818d77f3131e9087f4407443
paidforby <ggallo102@gmail.com>
2020-05-15 12:53:37 -0400

    update LoRaLayer2 commit related to #15

--- routers/fetch_deps.sh
+++ routers/fetch_deps.sh
@@ -8,7 +8,7 @@ mkdir -p libs
 cd libs/
 git clone https://github.com/sudomesh/LoRaLayer2
 cd LoRaLayer2/
-git checkout 6f6cc8b8fb2c5f22fc0dfc15c4fe96993ca3576d
+git checkout 1f488a9a6d0fb865018c3f316b0f23e12ed0bf42
 cd ../
 git clone https://github.com/bombela/backward-cpp
 cd backward-cpp
