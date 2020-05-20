68e65ca5aadab502b1062dc246cdd3a1cf94dc76
paidforby <ggallo102@gmail.com>
2020-05-20 10:54:57 -0400

    moved most of source back to main disaster radio repo

--- routers/fetch_deps.sh
+++ routers/fetch_deps.sh
@@ -6,12 +6,18 @@ rm -rf libs
 
 mkdir -p libs
 cd libs/
+
 git clone https://github.com/sudomesh/LoRaLayer2
 cd LoRaLayer2/
 git checkout 3f650076c1e1ff97d33c8aab20fb844528822542
 cd ../
+
+git clone https://github.com/sudomesh/disaster-radio
+cd disaster-radio/
+git checkout 875396a7c679d5105ec5e587ba295c2a486609b5
+cd ../
+
 git clone https://github.com/bombela/backward-cpp
 cd backward-cpp
 git checkout v1.5
 cd ../..
-
--- routers/utils/utils.h
+++
@@ -1,5 +0,0 @@
-//#include <Arduino.h>
-#include <stdio.h>
-
-void hexToChar(char * out, unsigned char * in, size_t insz);
-void charToHex(unsigned char * out, char * in, size_t outsz);
